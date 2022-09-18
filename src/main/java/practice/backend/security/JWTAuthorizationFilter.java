f005addb5b0ec045e1523013c44c0b028e01ba43
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-09-18 10:01:07 +0100

    fixing security

---
+++ src/main/java/practice/backend/security/JWTAuthorizationFilter.java
@@ -0,0 +1,65 @@
+package practice.backend.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import practice.backend.exception.BlogException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static practice.backend.security.SecurityConstants.*;
+
+@Slf4j
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String header = request.getHeader(HEADER_STRING);
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+        } else {
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = null;
+            try {
+                usernamePasswordAuthenticationToken = getAuthentication(request);
+            } catch (BlogException exception) {
+                exception.printStackTrace();
+            }
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            chain.doFilter(request, response);
+        }
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) throws BlogException {
+        String token = request.getHeader(HEADER_STRING);
+        if (token != null) {
+            String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .getSubject();
+            System.out.println(token);
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            throw new BlogException("User does not exist.");
+        }
+        else {
+            throw new BlogException("User does not exist.");
+        }
+    }
+}
+
--- src/main/java/practice/backend/security/SecurityConstants.java
+++ src/main/java/practice/backend/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package practice.backend.security;
 
 public class SecurityConstants {
-    public static final String SECRET = "zip";
+    public static final String SECRET = "blog_application";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
