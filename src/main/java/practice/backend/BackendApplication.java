a76f680eae4402f6a5dba9ae1049edf3b6a0ea64
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-09-21 03:18:25 +0100

    email sender fixed

--- src/main/java/practice/backend/BackendApplication.java
+++ src/main/java/practice/backend/BackendApplication.java
@@ -1,14 +1,27 @@
 package practice.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import practice.backend.service.email.EmailSenderService;
 
 @SpringBootApplication
 public class BackendApplication {
 
+    @Autowired
+    private EmailSenderService emailSenderService;
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
         System.out.println("Application is running on port 8080.");
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail(){
+        emailSenderService.sendEmail("okoroaforkelechi123@gmail.com", "Test",
+                "Hello, Did you see the test email?");
     }
 
 }
---
+++ src/main/java/practice/backend/service/email/EmailSenderService.java
@@ -0,0 +1,27 @@
+package practice.backend.service.email;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("okoroaforkelechi123@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+        log.info("Mail successfully sent.");
+    }
+}
