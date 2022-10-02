d08cc8e79e8992f7f8e0780e35b0f32191b7072d
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-02 08:29:24 +0100

    refactoring

--- src/main/java/practice/backend/BackendApplication.java
+++ src/main/java/practice/backend/BackendApplication.java
@@ -1,17 +1,10 @@
 package practice.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import practice.backend.service.email.EmailSenderService;
 
 @SpringBootApplication
 public class BackendApplication {
-
-    @Autowired
-    private EmailSenderService emailSenderService;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
